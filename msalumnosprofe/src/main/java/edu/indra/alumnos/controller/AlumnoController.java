a1a524cd7fda928d572b2f79bc427f029c3e5006
valexx55 <valexx55@gmail.com>
2022-10-17 14:01:26 +0200

    CRUD Alumno

---
+++ msalumnosprofe/src/main/java/edu/indra/alumnos/controller/AlumnoController.java
@@ -0,0 +1,128 @@
+package edu.indra.alumnos.controller;
+
+import java.util.Date;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.indra.alumnos.repository.entity.Alumno;
+import edu.indra.alumnos.service.AlumnoService;
+
+@RestController
+@RequestMapping("/alumno")
+public class AlumnoController {
+	
+	
+	@Autowired
+	AlumnoService alumnoService;
+	
+	//OBTENER TODOS LOS ALUMNOS -get x
+	//OBTENER UN ALUMNO POR ID -get x
+	//INSERTAR ALUMNO - post x
+	//MODIFICAR -put
+	//BORRAR -delete
+	
+	@GetMapping("/obtener-alumno-test") //GET http://localhost:8081/alumno/obtener-alumno-test
+	public Alumno obtenerAlumnoTest ()
+	{
+		Alumno alumno = null;
+		
+				alumno = new Alumno(1l, "Vale", "Moreno", "vale@gmail.es", 38, new Date());//TRANSIENT
+		
+		return alumno;
+	}
+	
+	@GetMapping //GET http://localhost:8081/alumno
+	public ResponseEntity<?> listarAlumnos ()
+	{
+		ResponseEntity<?> responseEntity = null;
+		Iterable<Alumno> listado_alumnos = null;
+		
+				listado_alumnos = this.alumnoService.findAll();
+				responseEntity = ResponseEntity.ok(listado_alumnos);
+		
+		return responseEntity;
+		
+	}
+	
+	@GetMapping("/{id}") //GET http://localhost:8081/alumno/1
+	public ResponseEntity<?> listarAlumnoPorId (@PathVariable Long id)
+	{
+		ResponseEntity<?> responseEntity = null;
+		Optional<Alumno> o_alumno = null;
+		
+				o_alumno = this.alumnoService.findById(id);
+				if (o_alumno.isPresent())
+				{
+					Alumno alumno_leido = o_alumno.get();
+					responseEntity = ResponseEntity.ok(alumno_leido);
+				} else {
+					responseEntity = ResponseEntity.noContent().build();//204
+				}
+				
+		
+		return responseEntity;
+		
+	}
+	
+	@PostMapping //POST http://localhost:8081/alumno
+	public ResponseEntity<?> insertarAlumno (@RequestBody Alumno alumno)
+	{
+		ResponseEntity<?> responseEntity = null;
+		Alumno alumno_creado = null;
+		
+			alumno_creado = this.alumnoService.save(alumno);
+			responseEntity = ResponseEntity.status(HttpStatus.CREATED).body(alumno_creado);//201
+			
+		
+		return responseEntity;
+		
+	}
+	
+	
+	@DeleteMapping("/{id}") //DELETE http://localhost:8081/alumno/1
+	public ResponseEntity<?> borrarAlumno (@PathVariable Long id)
+	{
+		ResponseEntity<?> responseEntity = null;
+		
+			this.alumnoService.deleteById(id);
+			responseEntity = ResponseEntity.ok().build();
+		
+		return responseEntity;
+		
+	}
+
+	
+	@PutMapping("/{id}") //PUT http://localhost:8081/alumno/1
+	public ResponseEntity<?> modificarAlumno (@RequestBody Alumno alumno, @PathVariable Long id)
+	{
+		ResponseEntity<?> responseEntity = null;
+		Optional<Alumno> o_alumno = null;
+		
+				o_alumno = this.alumnoService.update(alumno, id);
+				if (o_alumno.isPresent())
+				{
+					Alumno alumno_leido = o_alumno.get();
+					responseEntity = ResponseEntity.ok(alumno_leido);
+				} else {
+					responseEntity = ResponseEntity.notFound().build();//404
+				}
+				
+		
+		return responseEntity;
+		
+	}
+	
+	
+
+}
--- msalumnosprofe/src/main/java/edu/indra/alumnos/service/AlumnoServiceImpl.java
+++ msalumnosprofe/src/main/java/edu/indra/alumnos/service/AlumnoServiceImpl.java
@@ -4,39 +4,48 @@ import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import edu.indra.alumnos.repository.AlumnoRepository;
 import edu.indra.alumnos.repository.entity.Alumno;
 
+@Service
 public class AlumnoServiceImpl implements AlumnoService {
 	
 	@Autowired
 	AlumnoRepository alumnoRepository;
 
+	
 	@Override
+	@Transactional(readOnly = true)
 	public Iterable<Alumno> findAll() {
 		return this.alumnoRepository.findAll();
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public Optional<Alumno> findById(Long id) {
 		
 		return this.alumnoRepository.findById(id);
 	}
 
 	@Override
+	@Transactional
 	public Alumno save(Alumno alumno) {
 		
 		return this.alumnoRepository.save(alumno);
 	}
 
 	@Override
+	@Transactional
 	public void deleteById(Long id) {
 		this.alumnoRepository.deleteById(id);
 		
 	}
 
 	@Override
+	@Transactional
 	public Optional<Alumno> update(Alumno alumno, Long id) {
 		Optional<Alumno> optional = Optional.empty();
 		
@@ -44,9 +53,11 @@ public class AlumnoServiceImpl implements AlumnoService {
 			Optional<Alumno> aOptional = this.alumnoRepository.findById(id);
 			if (aOptional.isPresent())
 			{
-				Alumno alumno_leido = aOptional.get();
+				Alumno alumno_leido = aOptional.get();//PERSISTENCE
 				//alumno_leido.setNombre(alumno.getNombre());
 				BeanUtils.copyProperties(alumno, alumno_leido, "id", "creadoEn");
+				//this.alumnoRepository.save(alumno_leido);
+				optional = Optional.of(alumno_leido);
 			}
 			//si existe, lo modifico
 		
